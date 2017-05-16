36712c23e5e409b3a9c1abc3bf3ccfa31c228fd4
benloisch <ben.loisch@gmail.com>
2017-05-16 19:43:39 -0400

    tutorial 2

--- javaSoftwareRenderingTutorial_TheBennyBox/src/Main.java
+++ javaSoftwareRenderingTutorial_TheBennyBox/src/Main.java
@@ -2,7 +2,7 @@
  * Created by Ben Loisch on 5/16/2017.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         Display display = new Display(800, 600, "Software rendering");
     }
 }
